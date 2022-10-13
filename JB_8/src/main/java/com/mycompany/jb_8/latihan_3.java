3536e3b3de49a12ffe03d3e0c6e94d9e3bc935ce
21343063 <112668936+21343063@users.noreply.github.com>
2022-10-13 20:06:08 +0700

    Add files via upload

---
+++ JB_8/src/main/java/com/mycompany/jb_8/latihan_3.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.mycompany.jb_8;
+
+/**
+ *
+ * created by 21343063_Nurul Husna
+ */
+public class latihan_3 {
+    public static void main(String[]args){
+        String[][]dogs = {{"Terry","brown"},
+                          {"Kristin","white"},
+                          {"Toby", "gray"},
+                          {"Fido", "black"}
+                          };
+        System.out.println(dogs[0][0]);
+    }
+}
